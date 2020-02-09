362eb7ddde0956c22decc9b09b107d706b8bf041
Rony Krell <ronykrell@pm.me>
2020-02-09 08:30:53 -0500

    websockets sunday am

--- Handy/index.js
+++ Handy/index.js
@@ -77,10 +77,102 @@ function runAnimation(index, selectedActual, selectedPred, correctCount, incorre
 }
 
 $(document).ready(function() {
+    websocket = new WebSocket("ws://localhost:8765/");
+    console.log(websocket.readyState);
+    correctCount = 0
+    incorrectCount = 0
+    websocket.onmessage = function (event) {
+       console.log("On message")
+       response = JSON.parse(event.data);
+       if(!response.pred){
+         // if users msg
+        return
+       }
+       console.log(event);
+//        $(selectedActual).css("border", "10px solid transparent")
+//        $(selectedPred).css("border", "10px solid transparent")
+        $("div:has(img)").css("border", "10px solid transparent")
+        selectedPred = null
+        selectedActual = null
+        switch(response.actual) {
+            case 1:
+                selectedActual = "#cyrindarical_act"
+                break;
+            case 2:
+                selectedActual = "#tip_act"
+                break;
 
+            case 3:
+                selectedActual = "#hook_act"
+                break;
+
+            case 4:
+                selectedActual = "#palmar_act"
+                break;
+
+            case 5:
+                selectedActual = "#spherical_act"
+                break;
+
+            case 6:
+                selectedActual = "#lateral_act"
+        }
+        switch(response.pred) {
+            case 1:
+                selectedPred = "#cyrindarical_pred"
+                break;
+            case 2:
+                selectedPred = "#tip_pred"
+                break;
+            case 3:
+                selectedPred = "#hook_pred"
+                break;
+            case 4:
+                selectedPred = "#palmar_pred"
+                break;
+            case 5:
+                selectedPred = "#spherical_pred"
+                break;
+            case 6:
+                selectedPred = "#lateral_pred"
+                break;
+        }
+
+        if (response.pred === response.actual) {
+            $(selectedActual).css({"border": "10px solid green"})
+            $(selectedPred).css({"border": "10px solid green"})
+            correctCount++
+            $('#correct-count').text(correctCount);
+
+        }
+        else {
+            $(selectedActual).css({"border": "10px solid red"})
+            $(selectedPred).css({"border": "10px solid red"})
+            incorrectCount++
+           $('#incorrect-count').text(incorrectCount);
+
+        }
+        console.log(response.pred);
+        console.log(response.actual);
+        console.log(selectedActual);
+        console.log(selectedPred);
+
+     };
+     websocket.onopen = function(event){
+        console.log("Opened");
+        console.log(event);
+     }
+      websocket.onerror = function(event){
+        console.log("Error");
+        console.log(event);
+     }
+       websocket.onclose = function(event){
+        console.log("Closed");
+        console.log(event);
+     }
     $("#predict").click(function(){
-        console.log("hi");
-
-        runAnimation(0, null,  null, 0 , 0);
+       websocket.send("slow");
+//        console.log("hi");
+//      runAnimation(0, null,  null, 0 , 0);
     })
 })
--- garlic_spread.py
+++ garlic_spread.py
@@ -1,9 +1,95 @@
+#!/usr/bin/env python
+
 from flask import Flask, request, redirect, url_for
 from flask_restful import Resource, Api
 from joblib import load
 import numpy as np
 import pandas as pd
 import sklearn
+import json
+import asyncio
+import websockets
+import logging
+
+import time
+
+
+logging.basicConfig()
+
+STATE = {"value": 0}
+
+USERS = set()
+
+
+def state_event():
+    return json.dumps({"type": "state", **STATE})
+
+
+def users_event():
+    return json.dumps({"type": "users", "count": len(USERS)})
+
+
+async def notify_new_data(data):
+    if USERS:  # asyncio.wait doesn't accept an empty list
+        json_data = json.dumps(data)
+        await asyncio.wait([user.send(json_data) for user in USERS])
+
+
+async def notify_users():
+    if USERS:  # asyncio.wait doesn't accept an empty list
+        message = users_event()
+        await asyncio.wait([user.send(message) for user in USERS])
+
+
+async def register(websocket):
+    USERS.add(websocket)
+    await notify_users()
+
+
+async def unregister(websocket):
+    USERS.remove(websocket)
+    await notify_users()
+
+#
+# async def counter(websocket, path):
+#     # register(websocket) sends user_event() to websocket
+#     await register(websocket)
+#     try:
+#         await websocket.send(state_event())
+#         async for message in websocket:
+#             data = json.loads(message)
+#             if data["action"] == "minus":
+#                 STATE["value"] -= 1
+#                 await notify_state()
+#             elif data["action"] == "plus":
+#                 STATE["value"] += 1
+#                 await notify_state()
+#             else:
+#                 logging.error("unsupported event: {}", data)
+#     finally:
+#         await unregister(websocket)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 from flask_cors import CORS, cross_origin
 
 # Upload folder
@@ -12,12 +98,12 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
 
-
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['CORS_HEADERS'] = 'Content-Type'
-CORS(app)
-
-api = Api(app)
+#
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# app.config['CORS_HEADERS'] = 'Content-Type'
+# CORS(app)
+#
+# api = Api(app)
 
 latest_pred = 0
 latest_actual = 0
@@ -28,8 +114,8 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-def get_prediction(json_row):
-    # json_row = json.loads(feature_row)
+def get_prediction(feature_row):
+    json_row = json.loads(feature_row)
     features_array = json_row['feature_row'][:10]
     actual_movement = json_row['feature_row'][-1]
     np_arr = np.array(features_array).reshape(1,-1)
@@ -77,10 +163,127 @@ class MovementPrediction(Resource):
             return global_history[ind_num]
 
 
-api.add_resource(MovementPrediction, '/predict/')
+# api.add_resource(MovementPrediction, '/predict/')
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+
+# WS server example
+
+async def hello(websocket, path):
+    name = await websocket.recv()
+    print(f"< {name}")
+
+    greeting = f"Hello {name}!"
+
+    await websocket.send(greeting)
+    print(f"> {greeting}")
+
+
+async def save_the_data(message):
+    # feature_row = await websocket.recv()
+    feature_row = message
+    print(feature_row)
+    pred_obj = get_prediction(feature_row)
+    global_latest_pred = pred_obj['pred']
+    global_latest_actual = pred_obj['actual']
+    global_history.append({'pred': global_latest_pred, 'actual': global_latest_actual})
+    print("Added point")
+    print(len(global_history))
+    # print("Got")
+    # print(f"< {name}")
+    # await websocket.send("Got it")
+    # print(f"> {greeting}")
+
+
+async def save_data(websocket, path):
+    try:
+        delay = 0
+        await register(websocket)
+        async for message in websocket:
+            if message == "slow":
+                delay = .5
+            else:
+                await save_the_data(message)
+                time.sleep(delay)
+                await notify_new_data(global_history[-1])
+    finally:
+        await unregister(websocket)
+
+
+
+
+
+print("Im alive")
+
+start_server = websockets.serve(save_data, "localhost", 8765, ping_timeout=None)
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
+
+
+
+# if __name__ == '__main__':
+    # app.run(debug=True, host='0.0.0.0')
+
+
+logging.basicConfig()
+
+STATE = {"value": 0}
+
+USERS = set()
+
+
+def state_event():
+    return json.dumps({"type": "state", **STATE})
+
+
+def users_event():
+    return json.dumps({"type": "users", "count": len(USERS)})
+
+
+async def notify_state():
+    if USERS:  # asyncio.wait doesn't accept an empty list
+        message = state_event()
+        await asyncio.wait([user.send(message) for user in USERS])
+
+
+async def notify_users():
+    if USERS:  # asyncio.wait doesn't accept an empty list
+        message = users_event()
+        await asyncio.wait([user.send(message) for user in USERS])
+
+
+async def register(websocket):
+    USERS.add(websocket)
+    await notify_users()
+
+
+async def unregister(websocket):
+    USERS.remove(websocket)
+    await notify_users()
+
+
+async def counter(websocket, path):
+    # register(websocket) sends user_event() to websocket
+    await register(websocket)
+    try:
+        await websocket.send(state_event())
+        async for message in websocket:
+            data = json.loads(message)
+            if data["action"] == "minus":
+                STATE["value"] -= 1
+                await notify_state()
+            elif data["action"] == "plus":
+                STATE["value"] += 1
+                await notify_state()
+            else:
+                logging.error("unsupported event: {}", data)
+    finally:
+        await unregister(websocket)
+
+
+start_server = websockets.serve(counter, "localhost", 6789)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
 
 
 
